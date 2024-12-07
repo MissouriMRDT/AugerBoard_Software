9349c5e3f97aa21f7fa44f25c3ad9231642a4a62
aespinoza1737 <aespinoza1737@gmail.com>
2024-12-07 00:23:04 -0600

    lots of stuff done ish hi

--- include/AugerBoard.h
+++ include/AugerBoard.h
@@ -4,6 +4,75 @@
 #include <Arduino.h>
 #include "PinAssignments.h"
 #include <RoveComm.h>
+#include <RoveMotor.h>
+#include <RoveQuadEncoder.h>
+#include <RoveJoint.h>
+#include <LimitSwitch.h>
+#include <RoveVNH.h>
 
 
+//temp and humidity max min shit and dividers idk hai
+
+// RoveComm
+RoveCommEthernet RoveComm;
+
+// Watchdog
+#define WATCHDOG_TIMEOUT 300000
+IntervalTimer Watchdog;
+uint8_t watchdogStatus = 0;
+uint8_t watchdogOverride = 0;
+
+#define TELEMETRY_INTERVAL 300000
+IntervalTimer Telemetry;
+
+// Motors
+
+RoveVNH AugerAxisMotor(PWM1,IN_A1,IN_B1,CS1);
+RoveVNH AugerMotor(PWM2,IN_A2,IN_B2,CS2);
+RoveVNH SpareMotor(PWM3,IN_A3,IN_B3,CS3);
+
+// Encoders
+RoveQuadEncoder AugerAxisEncoder(ENCODER_2A , ENCODER_2B, 360);
+RoveQuadEncoder AugerEncoder(ENCODER_1A, ENCODER_1B, 360);
+
+
+// Limit Switches
+LimitSwitch AugerAxisRVSLimit(LIMITSWITCH1);
+LimitSwitch AugerAxisFWDLimit(LIMITSWITCH2);
+LimitSwitch AugerAxisRVSLimitSpare(LIMITSWITCH3);
+LimitSwitch AugerAxisFWDLimitSpare(LIMITSWITCH4);
+
+// Joints
+RoveJoint AugerAxis(&AugerAxisMotor);
+
+
+// Control variables
+int16_t augerDecipercent = 0;
+int16_t augerAxisDecipercent = 0;
+
+// Methods
+// analogMap function eurm
+float analogMap(uint16_t measurement, uint16_t fromADC, uint16_t toADC, float fromAnalog, float toAnalog);
+void telemetry();
+void feedWatchdog();
+void estop();
+
+
+// this is rlly awkward :grimacing:
+#define RC_AUGERBOARD_IPADDRESS RC_SCIENCEACTUATIONBOARD_IPADDRESS
+
+#define RC_AUGERBOARD_LIMITSWITCHOVERRIDE_DATA_ID RC_SCIENCEACTUATIONBOARD_LIMITSWITCHOVERRIDE_DATA_ID
+#define RC_AUGERBOARD_AUGER_DATA_ID RC_SCIENCEACTUATIONBOARD_AUGER_DATA_ID
+#define RC_AUGERBOARD_REQUESTHUMIDITY_DATA_ID RC_SCIENCEACTUATIONBOARD_REQUESTHUMIDITY_DATA_ID
+#define RC_AUGERBOARD_WATCHDOGOVERRIDE_DATA_ID RC_SCIENCEACTUATIONBOARD_WATCHDOGOVERRIDE_DATA_ID
+
+#define RC_AUGERBOARD_POSITION_DATA_ID 69
+#define RC_AUGERBOARD_POSITION_DATA_COUNT 1
+#define RC_AUGERBOARD_POSITION_DATA_TYPE float
+
+#define RC_AUGERBOARD_WATCHDOGSTATUS_DATA_ID 420
+
+#define RC_AUGERBOARD_LIMITSWITCHTRIGGERED_DATA_ID 123456789
+
+#define RC_AUGERBOARD_AUGERAXIS_OPENLOOP_DATA_ID 987654321
 #endif
--- src/AugerBoard.cpp
+++ src/AugerBoard.cpp
@@ -1,8 +1,6 @@
 #include "AugerBoard.h"
 
 void setup() {
-    // put your setup code here, to run once:
-
     Serial.begin(115200);
 
     // Test Buttons
@@ -12,8 +10,118 @@ void setup() {
 
     // Direction Switch
     pinMode(DIR_SW, INPUT); 
+
+    AugerAxis.attachEncoder(&AugerAxisEncoder);
+
+    AugerAxis.attachHardLimits(&AugerAxisRVSLimit, &AugerAxisFWDLimit);
+
+    AugerAxis.Motor()->configRampRate(5000);
+    AugerMotor.configRampRate(5000);
+
+    AugerAxisEncoder.begin([]{ AugerAxisEncoder.handleInterrupt(); });
+    AugerEncoder.begin([]{ AugerAxisEncoder.handleInterrupt(); });
+
+    AugerAxis.Motor()->configMaxOutputs(-1000,1000);
+    AugerAxis.Motor()->configMinOutputs(0,0);
+
+    Serial.println("RoveComm Initializing...");
+    RoveComm.begin(RC_AUGERBOARD_IPADDRESS);
+    Telemetry.begin(telemetry,TELEMETRY_INTERVAL);
+    Serial.println("Complete");
+
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+
+    RoveCommPacket packet = RoveComm.read();
+
+    switch (packet.dataId){
+        case RC_AUGERBOARD_AUGERAXIS_OPENLOOP_DATA_ID:
+        {
+            augerAxisDecipercent = *((int16_t*) packet.data);
+            feedWatchDog();
+
+            break;
+        }
+        case RC_AUGERBOARD_LIMITSWITCHOVERRIDE_DATA_ID:
+        {
+            uint8_t data = ((uint8_t*) packet.data)[0];
+
+            AugerAxis.overrideForwardHardLimit(data & (1<<0));
+            AugerAxis.overrideReverseHardLimit(data & (1<<1));
+
+            break;
+        }
+
+        case RC_AUGERBOARD_REQUESTHUMIDITY_DATA_ID:
+        {
+            // uhh bunch of shit tbh
+
+            break;
+        }
+        case RC_AUGERBOARD_AUGER_DATA_ID:
+        {
+            augerDecipercent = *((int16_t*) packet.data);
+            feedWatchDog();
+
+            break;
+        }
+        case RC_AUGERBOARD_WATCHDOGOVERRIDE_DATA_ID:
+        {
+            watchdogOverride = *((uint8_t*) packet.data);
+
+            break;
+        }
+    }
+
+    bool direction = digitalRead(DIR_SW);
+
+    // AugerAxis
+    if(!digitalRead(SW2)) AugerAxis.drive((direction ? -900 : 900));
+    else AugerAxis.drive(augerAxisDecipercent);
+    // Auger
+    if(!digitalRead(SW1)) AugerAxis.drive((direction ? -900 : 900));
+    else AugerMotor.drive(augerDecipercent);
+    // Spare Motor
+    if(!digitalRead(SW3)) SpareMotor.drive((direction ? -900 : 900));
+    else SpareMotor.drive(0);
 }
+
+
+void telemetry(){
+
+    // .write() vs .writeReliable()
+
+    // Encoder position
+    float position = AugerAxisEncoder.readDegrees();
+    RoveComm.writeReliable(RC_AUGERBOARD_POSITION_DATA_ID,position);
+
+    // Limit
+    uint8_t limitSwitchValues = (AugerAxis.atForwardHardLimit()) | (AugerAxis.atReverseHardLimit());
+    RoveComm.writeReliable(RC_AUGERBOARD_LIMITSWITCHTRIGGERED_DATA_ID,limitSwitchValues);
+
+    // Watchdog status
+    RoveComm.writeReliable(RC_AUGERBOARD_WATCHDOGSTATUS_DATA_ID,watchdogStatus);
+
+}
+float analogMap(){
+    
+}
+
+void estop(){
+    watchdogStatus = 1;
+    if(!watchdogOverride){
+        AugerAxis.drive(0);
+        AugerMotor.drive(0); 
+
+        augerAxisDecipercent = 0;
+        augerDecipercent = 0;
+    }
+
+}
+
+void feedWatchDog(){
+    watchdogStatus = 0;
+    Watchdog.begin(estop,WATCHDOG_TIMEOUT);
+}
+
