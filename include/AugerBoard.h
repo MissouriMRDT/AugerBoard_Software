4b0235a8e8e4d4b277ee2be81f052703b35a5daa
blaydnbuckwalter-cmd <blaydnbuckwalter@gmail.com>
2025-12-05 00:34:59 -0600

    Added Basic Code for setup and loop

---
+++ include/AugerBoard.h
@@ -0,0 +1,12 @@
+#ifndef AUGERBOARD_H
+#define AUGERBOARD_H
+
+#include "PinAssignments.h"
+#include <PWMServo.h>
+#include <RoveComm.h>
+#include <arduino.h>
+
+// RoveComm
+RoveCommEthernet RoveComm;
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,61 @@
-#include <Arduino.h>
+#include "AugerBoard.h"
 
 void setup() {
     // put your setup code here, to run once:
+    Serial.begin(115200);
+
+    // Test Buttons
+    pinMode(GIMBAL_TILT_SW, INPUT_PULLUP);
+    pinMode(AF_SPARE_SW, INPUT_PULLUP);
+    pinMode(S_CACHE_SPARE_SW, INPUT_PULLUP);
+    pinMode(SOIL_DOOR_SW, INPUT_PULLUP);
+    pinMode(GANTRY_SW, INPUT_PULLUP);
+    pinMode(GIMBAL_PAN_SW, INPUT_PULLUP);
+    pinMode(SPARE_SW_12V, INPUT_PULLUP);
+    pinMode(AUGER_SW, INPUT_PULLUP);
+    pinMode(AF_LENS_SW, INPUT_PULLUP);
+
+    // Direction Switch
+    pinMode(DIR_SW, INPUT);
+
+    // Sensors
+    pinMode(TEMP, INPUT);
+    pinMode(MOISTURE, INPUT);
+
+    // Autoflouresence
+    pinMode(AF_LED_365, OUTPUT);
+    pinMode(AF_LED_405, OUTPUT);
+    pinMode(AF_LED_490, OUTPUT);
+    pinMode(AF_WHITE_LED, OUTPUT);
+
+    RoveComm.begin(RC_AUGERBOARD_IPADDRESS);
 }
 
+RoveCommPacket packet;
 void loop() {
-    // put your main code here, to run repeatedly:
+    RoveComm.read(packet);
+
+    switch (packet.dataId) {
+    case RC_AUGERBOARD_AUGERAXIS_OPENLOOP_DATA_ID: {
+        break;
+    }
+    case RC_AUGERBOARD_LIMITSWITCHOVERRIDE_DATA_ID: {
+        break;
+    }
+    case RC_AUGERBOARD_WATCHDOGOVERRIDE_DATA_ID: {
+        break;
+    }
+    case RC_AUGERBOARD_REQUESTTEMPERATURE_DATA_ID: {
+        break;
+    }
+    case RC_AUGERBOARD_REQUESTHUMIDITY_DATA_ID: {
+        break;
+    }
+    case RC_AUGERBOARD_UVLED_DATA_ID: {
+        break;
+    }
+    case RC_AUGERBOARD_AUGER_DATA_ID: {
+        break;
+    }
+    }
 }
